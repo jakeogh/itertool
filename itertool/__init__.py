c0f7c3e99a341e7a9823c824d36c47aa1f732f2c
jakeogh <github.com@v6y.net>
2021-10-01 13:41:48 -0700

    auto-commit

---
+++ itertool/__init__.py
@@ -0,0 +1,1 @@
+#from .itertool import itertool
---
+++ itertool/itertool.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+# flake8: noqa           # flake8 has no per file settings :(
+# pylint: disable=C0111  # docstrings are always outdated and wrong
+# pylint: disable=C0114  #      Missing module docstring (missing-module-docstring)
+# pylint: disable=W0511  # todo is encouraged
+# pylint: disable=C0301  # line too long
+# pylint: disable=R0902  # too many instance attributes
+# pylint: disable=C0302  # too many lines in module
+# pylint: disable=C0103  # single letter var names, func name too descriptive
+# pylint: disable=R0911  # too many return statements
+# pylint: disable=R0912  # too many branches
+# pylint: disable=R0915  # too many statements
+# pylint: disable=R0913  # too many arguments
+# pylint: disable=R1702  # too many nested blocks
+# pylint: disable=R0914  # too many local variables
+# pylint: disable=R0903  # too few public methods
+# pylint: disable=E1101  # no member for base
+# pylint: disable=W0201  # attribute defined outside __init__
+# pylint: disable=R0916  # Too many boolean expressions in if statement
+# pylint: disable=C0305  # Trailing newlines editor should fix automatically, pointless warning
+# pylint: disable=C0413  # TEMP isort issue [wrong-import-position] Import "from pathlib import Path" should be placed at the top of the module [C0413]
+
+
+import os
+#import time
+#import sh
+from signal import SIG_DFL
+from signal import SIGPIPE
+from signal import signal
+
+#import sys
+import click
+
+signal(SIGPIPE, SIG_DFL)
+from itertools import zip_longest
+from pathlib import Path
+from typing import ByteString
+from typing import Generator
+from typing import Iterable
+from typing import List
+from typing import Optional
+from typing import Sequence
+from typing import Tuple
+from typing import Union
+
+from asserttool import eprint
+from asserttool import ic
+from asserttool import nevd
+from asserttool import validate_slice
+#from asserttool import verify
+#from retry_on_exception import retry_on_exception
+from enumerate_input import enumerate_input
+
+
+def true_items_in_iterator(iterator, verbose=False):
+    if verbose:
+        ic(iterator)
+    answer = sum(x for x in iterator if x is True)
+    return answer
+
+
+# https://docs.python.org/3/library/itertools.html#itertools-recipes
+def grouper(iterable, n, fillvalue=None):
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
+def compact(items):
+    return [item for item in items if item]
+
+
+@click.command()
+@click.argument("paths", type=str, nargs=-1)
+@click.argument("sysskel",
+                type=click.Path(exists=False,
+                                dir_okay=True,
+                                file_okay=False,
+                                allow_dash=False,
+                                path_type=Path,),
+                nargs=1,
+                required=True,)
+@click.argument("slice_syntax", type=validate_slice, nargs=1)
+@click.option('--verbose', is_flag=True)
+@click.option('--debug', is_flag=True)
+@click.option('--simulate', is_flag=True)
+@click.option('--ipython', is_flag=True)
+@click.pass_context
+def cli(ctx,
+        paths: Optional[tuple[str]],
+        sysskel: Path,
+        slice_syntax: str,
+        verbose: bool,
+        debug: bool,
+        simulate: bool,
+        ipython: bool,
+        ):
+
+    null, end, verbose, debug = nevd(ctx=ctx,
+                                     printn=False,
+                                     ipython=False,
+                                     verbose=verbose,
+                                     debug=debug,)
+
+    iterator = paths
+    del paths
+
+    index = 0
+    for index, path in enumerate_input(iterator=iterator,
+                                       dont_decode=True,  # paths are bytes
+                                       progress=False,
+                                       debug=debug,
+                                       verbose=verbose,):
+        path = Path(os.fsdecode(path))
+
+        if verbose:  # or simulate:
+            ic(index, path)
